dca208730e0ba67cc4baf8a023d796d37e421d33
saivardhanbabu22 <saivardhanbabu159@gmail.com>
2025-02-26 11:00:29 +0530

    commit-24

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const expressAsynHandler = require('express-async-handler');
 app.use(exp.json());
-app.use(exp.static(path.join(__dirname, '../whack-a-mole/build')));
+app.use(exp.static(path.join(__dirname, 'whack-a-mole/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../whack-a-mole/build', 'index.html'));
